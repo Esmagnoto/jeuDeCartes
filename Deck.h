8cf6d1e973346fe3eeb8d150b8629e3ae299b364
Caio <esmagnotocaio@gmail.com>
2023-12-16 17:20:37 +0100

    changement 1

---
+++ Deck.h
@@ -0,0 +1,38 @@
+/**
+ * @author Caio and Jules
+ * @version 16122023
+ **/
+
+#ifndef JEUDECARTES_DECK_H
+#define JEUDECARTES_DECK_H
+
+
+
+struct Deck{
+    int taille; // Combien des cartes dans le deck = 52
+    int nextCarte; // Index de la prochaine carte a etre distribuee
+    int carteLeft; // Nombre de carte restents a distribuer
+    Carte* cartes;
+};
+
+/**
+ * @brief Initialiser le Deck des cartes,
+ * @param[in,out] deck : la pile
+ * @param[in] :
+ * @pre pre condition;
+ */
+void initialiser();
+
+void melanger();
+
+void distribuerCartes();
+
+
+
+
+
+
+
+
+
+#endif //JEUDECARTES_DECK_H
